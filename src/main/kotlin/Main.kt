e28cc0cc3aaa588747b4cbc2f8099998a3ca5a26
Jose Antonio <jdiabus511@g.educaand.es>
2024-03-11 19:41:43 +0100

    Enemigo final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package org.practicatrim2
 fun main() {
 
     val mbappe = Jugador("Mbappé", Tipo("Electrico"),350,91)
-    val alkhelaifi = Jugador("Alkhelaifi",Tipo("Roca"),370,14)
+    val alkhelaifi = Enemigo("Alkhelaifi",Tipo("Roca"),370,140)
 
     val jugador1 = mbappe
     val jugador2 = alkhelaifi
